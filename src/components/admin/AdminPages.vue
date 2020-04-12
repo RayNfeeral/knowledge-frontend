f540f7660594e441073d0c30c7a80171405e7b87
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2020-04-12 07:43:44 -0300

    initial commit

---
+++ src/components/admin/AdminPages.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <PageTittle icon="fa fa-cogs" main="Administração"
+            sub="Cadastros & Cia" />
+        <div class="admin-pages-tabs">
+            <b-card no-body>
+                <b-tabs card>
+                    <b-tab title='Artigos' active>
+                        <ArticleAdmin />
+                    </b-tab>
+                    <b-tab title='Categorias'>
+                        <CategoryAdmin />
+                    </b-tab>
+                    <b-tab title='Usuários'>
+                        <UserAdmin />
+                    </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
+    </div>
+</template>
+
+<script>
+import PageTittle from '../template/PageTittle'
+import ArticleAdmin from './ArticleAdmin'
+import CategoryAdmin from './CategoryAdmin'
+import UserAdmin from './UserAdmin'
+
+export default {
+    name: 'AdminPages',
+    components: { PageTittle, ArticleAdmin, CategoryAdmin, UserAdmin }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/admin/CategoryAdmin.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="category-admin">
+    <b-form>
+      <input type="hidden" id="category-id" v-model="category.id" />
+      <b-form-group label="Nome:" label-for="category-name">
+        <b-form-input
+          id="category-name"
+          type="text"
+          v-model="category.name"
+          required
+          :readonly="mode === 'remove'"
+          placeholder="Informe o Nome da Categoria..."
+        />
+      </b-form-group>
+      <b-form-group label="Categoria Pai:" label-for="category-parentId">
+        <b-form-select
+          v-if="mode === 'save'"
+          id="category-parentId"
+          v-model="category.parentId"
+          :options="allCategories"
+        />
+        <b-form-input
+          v-else
+          id="category-parentId"
+          type="text"
+          v-model="category.path"
+          readonly
+        />
+      </b-form-group>
+      <b-row>
+        <b-col xs="12">
+          <b-button variant="primary" v-if="mode === 'save'" @click="save">Salvar</b-button>
+          <b-button variant="danger" v-if="mode === 'remove'" @click="remove">Excluir</b-button>
+          <b-button class="ml-2" @click="reset">Cancelar</b-button>
+        </b-col>
+      </b-row>
+    </b-form>
+    <hr />
+    <b-table hover striped :items="categories" :fields="fields">
+      <template slot="actions" slot-scope="data">
+        <b-button variant="warning" @click="loadCategory(data.item)" class="mr-2">
+          <i class="fa fa-pencil"></i>
+        </b-button>
+        <b-button variant="danger" @click="loadCategory(data.item, 'remove')">
+          <i class="fa fa-trash"></i>
+        </b-button>
+      </template>
+    </b-table>
+    <b-pagination size="md" v-model="page" :total-rows="count" :per-page="limit"/>
+  </div>
+</template>
+
+<script>
+import { baseApiUrl, showError } from "@/global";
+import axios from "axios";
+
+export default {
+  name: "CategoryAdmin",
+  data: function() {
+    return {
+      mode: "save",
+      category: {},
+      categories: [],
+      allCategories: [],
+      page: 1,
+      count: 0,
+      limit: 0,
+      fields: [
+        { key: "id", label: "Código", sortable: true },
+        { key: "name", label: "Nome", sortable: true },
+        { key: "path", label: "Caminho", sortable: true },
+        { key: "actions", label: "Ações" }
+      ]
+    };
+  },
+  methods: {
+    loadCategories(pagination = false) {
+      const url = `${baseApiUrl}/categories?pagination=${pagination}&&page=${this.page}`;
+      axios.get(url).then(res => {
+        // this.categories = res.data
+        this.categories = res.data.data.map(category => {
+          return { ...category, value: category.id, text: category.path };
+        });
+        this.count = res.data.count
+        this.limit = res.data.limit
+      });
+    },
+    loadAllCategories() {
+      const url = `${baseApiUrl}/categories`;
+      axios.get(url).then(res => {
+        // this.categories = res.data
+        this.allCategories = res.data.map(category => {
+          return { ...category, value: category.id, text: category.path };
+        });
+      });
+    },
+    reset() {
+      this.mode = "save";
+      this.category = {};
+      this.loadCategories();
+    },
+    save() {
+      const method = this.category.id ? "put" : "post";
+      const id = this.category.id ? `/${this.category.id}` : "";
+      axios[method](`${baseApiUrl}/categories${id}`, this.category)
+        .then(() => {
+          this.$toasted.global.defaultSuccess();
+          this.reset();
+        })
+        .catch(showError);
+    },
+    remove() {
+      const id = this.category.id;
+      axios
+        .delete(`${baseApiUrl}/categories/${id}`)
+        .then(() => {
+          this.$toasted.global.defaultSuccess();
+          this.reset();
+        })
+        .catch(showError);
+    },
+    loadCategory(category, mode = "save") {
+      this.mode = mode;
+      this.category = { ...category };
+    }
+  },
+  watch: {
+    page() {
+      this.loadCategories(true)
+    }
+  },
+  mounted() {
+    this.loadCategories(true);
+    this.loadAllCategories();
+  }
+};
+</script>
+
+<style>
+</style>
