981b96057b5d75f49fd498dbe0f42f00e0afd032
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2020-04-12 11:03:52 -0300

    Configurando o server da aplicacao(dessa vez vai)

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const port = process.env.PORT || 8080
+const serveStatic = require('serve-static')
+const path = require('path')
+
 const app = express()
 
-app.use(express.static("/dist/"))
-app.get(/.*/, (req, res) => {
-    res.sendfile(__dirname + "/dist/index.html")
-})
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
+const port = process.env.PORT || 8080
 app.listen(port, () => {
-    console.log("Frontend executando...")
+    console.log("Listening on port: " + port)
 })
