e1be2f28fab28bcbc7a2b53c6ae1e7ab0159cea7
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2020-04-12 08:06:09 -0300

    Change server

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const port = process.env.PORT || 8080
 const app = express()
 
-app.use(express.static("/dist/"))
+app.use('/', express.static("/dist/"))
+// app.get(/.*/)
 
 app.listen(port, () => {
     console.log("Frontend executando...")
