8a93a894541be3b67afa7833779c8aac1559b7a3
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2020-04-12 11:44:50 -0300

    ...

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.get('/*', (req, res) => {
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '/dist'))
 })
 
