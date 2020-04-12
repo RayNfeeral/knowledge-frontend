cda2810ad500e682c3ceb12daa8f05e55254e7c6
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2020-04-12 11:52:19 -0300

    ...

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const path = require('path')
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/dist'))
+
+app.get('/auth', function(req, res) {
+    console.log('Entrou em auth')
 })
 
 const port = process.env.PORT || 8080
