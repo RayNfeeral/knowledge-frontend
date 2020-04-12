06c28e5f410091dffa5b70079d9ceb630a65fd40
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2020-04-12 08:09:59 -0300

    Change server 2

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const port = process.env.PORT || 8080
 const app = express()
 
-app.use('/', express.static("/dist/"))
-// app.get(/.*/)
+app.use(express.static("/dist/"))
+app.get(/.*/, (req, res) => {
+    res.sendfile(__dirname + "/dist/index.html")
+})
 
 app.listen(port, () => {
     console.log("Frontend executando...")
